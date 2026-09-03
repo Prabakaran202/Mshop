727cb906668b28f2ac49faa98b136be7b2c47e58
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-09-03 11:14:42 +0530

    Update build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    // Ungaloda pazhaya dependencies kooda ithaiyum serthukkonga
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
